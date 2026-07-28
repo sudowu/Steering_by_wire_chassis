a5e9eccd3e4e8a8579f7a459e61dd2fafffd25a5
羊咩 <212190935@qq.com>
2026-07-28 17:12:29 +0800

    添加 can_drive_node 节点实现，更新 CMakeLists.txt 以构建可执行文件，添加控制库和相关依赖，更新 VSCode 配置文件

--- src/can_drive/src/can_drive.cpp
+++ src/can_drive/src/can_drive.cpp
@@ -1,3 +1,43 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "controlcan.h"
+
+class CanDriveNode : public rclcpp::Node
+{
+public:
+    CanDriveNode() : Node("can_drive_node")
+    {
+
+        publisher_ = this->create_publisher<std_msgs::msg::String>("can_drive_topic", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500),
+            std::bind(&CanDriveNode::publish_message, this));
+    }
+
+private:
+
+    void publish_message()
+    {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello from CanDriveNode!";
+        publisher_->publish(message);
+        RCLCPP_INFO(this->get_logger(), "Published message: '%s'", message.data.c_str());
+    }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    VCI_BOARD_INFO pInfo;//用来获取设备信息。
+    int count=0;//数据列表中，用来存储列表序号。
+    VCI_BOARD_INFO pInfo1 [50];
+
+};
 
 
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CanDriveNode>());
+    rclcpp::shutdown();
+return 0;
+}
