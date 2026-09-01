e64547e8e3a3a13fb0acff3f783d67506f0a9796
羊咩 <212190935@qq.com>
2026-09-01 16:25:53 +0800

    重构 CMakeLists.txt，移除 ament_target_dependencies，改用命名空间链接库

--- src/camera_drive/src/camera_drive.cpp
+++ src/camera_drive/src/camera_drive.cpp
@@ -1,7 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/header.hpp>
-#include <cv_bridge/cv_bridge.h>
+// Lyrical 中 cv_bridge 头文件已更名为 cv_bridge.hpp
+// #include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
 
 class ImagePublisher : public rclcpp::Node
